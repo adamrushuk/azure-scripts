9d5f8ed42585483c85bf0d7d5c688cdc3681b6b2
Adam Rush <adamrushuk@gmail.com>
2023-08-21 13:48:48 +0100

    Changed to use Microsoft Graph PowerShell SDK

--- powershell/Get-DuplicateAadGroups.ps1
+++ powershell/Get-DuplicateAadGroups.ps1
@@ -1,22 +1,41 @@
-# show duplicate aad groups
-# must be run from Windows PowerShell session
+# Finds duplicate AAD Groups or AAD Applications using the Microsoft Graph PowerShell SDK
+# https://learn.microsoft.com/en-us/powershell/microsoftgraph/installation?view=graph-powershell-1.0
 
 # install module
-Install-module AzureADPreview -Verbose
-Get-Module AzureADPreview -Verbose -ListAvailable
+Install-Module Az.Accounts, Microsoft.Graph.Authentication
+Get-Module Az.Accounts, Microsoft.Graph.Authentication -ListAvailable
 
 # login
 # open browser (but may not come into focus)
-Connect-AzureAD -Confirm
+Connect-AzAccount -UseDeviceAuthentication
 
-# get all aad groups
-$aadGroups = Get-AzureADGroup -All $true
-$aadGroups.Count
-$aadGroups | Sort-Object -Property DisplayName
+# check login context
+Get-AzContext
 
-# group aad groups
-$aadGroupsGrouped = $aadGroups | Group-Object -Property DisplayName
+# authenticate to the Microsoft Graph
+$accessToken = Get-AzAccessToken -ResourceTypeName 'MSGraph' -ErrorAction 'Stop'
+Connect-MgGraph -AccessToken $accessToken.Token
 
-# find duplicate group names
-$aadGroupsGroupedDupes = $aadGroupsGrouped | Where-Object Count -GT 1
-$aadGroupsGroupedDupes | Sort-Object -Property Count -Descending | Format-Table -AutoSize
+# see debugging output of the command by adding "-Debug"
+# Get-MgUser -Top 10 -Debug
+# Get-MgApplication -Top 10 -Debug
+Get-MgGroup -Top 10 # -Debug
+
+# choose one of the following AD objects
+# $aadObjects = Get-MgApplication -All
+$aadObjects = Get-MgGroup -All
+
+# check AD objects
+$aadObjects.Count
+$aadObjects | Sort-Object -Property DisplayName
+
+# group AD objects
+$aadObjectsGrouped = $aadObjects | Group-Object -Property DisplayName
+
+# find duplicates
+$aadObjectsGroupedDupes = $aadObjectsGrouped | Where-Object Count -GT 1
+$aadObjectsGroupedDupes | Sort-Object -Property Count -Descending | Format-Table -AutoSize
+
+# display info
+$aadObjectsGroupedDupes.Group | Format-Table -AutoSize
+$aadObjectsGroupedDupes.Group | Format-Table -AutoSize | clip.exe
